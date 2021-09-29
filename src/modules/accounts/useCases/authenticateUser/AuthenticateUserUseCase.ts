a3a226831793157d8c473104a243fc49df26f8cb
Beatriz Murase <biamurase@gmail.com>
2021-09-28 22:04:15 -0300

    :rocket: create authenticate user use case

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,48 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import IAuthenticateUserDTO from '../../dtos/IAuthenticateUserDTO';
+import IUsersRepository from '../../repositories/IUsersRepository';
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ) {}
+
+    public async execute({
+        email,
+        password,
+    }: IAuthenticateUserDTO): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Incorrect email or password!');
+        }
+
+        const passwordMatches = await compare(password, user.password);
+
+        if (!passwordMatches) {
+            throw new Error('Incorrect email or password!');
+        }
+
+        const token = sign({}, 'dss65afcvs2daf8c64x8s964gnsdg', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return { user, token } as IResponse;
+    }
+}
+
+export { AuthenticateUserUseCase };
