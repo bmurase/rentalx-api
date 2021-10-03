307cdc9b6c1c2f71c4a35734e2dfd43ed0991827
Beatriz Murase <biamurase@gmail.com>
2021-10-03 17:57:08 -0300

    :rocket: create upload config file & update avatar route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
 
+import uploadConfig from '../config/upload';
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController';
 
 const categoriesRouter = Router();
 
-const upload = multer({
-    dest: './tmp',
-});
+const upload = multer(uploadConfig.upload('./tmp'));
 
 const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
