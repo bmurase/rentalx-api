f3a2611efc2be681a798b4f4f0136ddc2901de74
Beatriz Murase <biamurase@gmail.com>
2021-10-13 23:20:17 -0300

    :rocket: create rentals table

--- src/shared/infra/typeorm/migrations/1632699766763-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1632699766763-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1632699766763 implements MigrationInterface {
                     {
                         name: 'id',
                         type: 'uuid',
+                        isPrimary: true,
                     },
                     {
                         name: 'name',
